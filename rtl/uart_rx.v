d223a5e377da9e6784729c0ebe6835dd88052a33
Dave Keeshan <dave.keeshan@daxzio.com>
2023-08-01 12:11:25 +0100

    first attempt

--- rtl/uart_rx.v
+++ rtl/uart_rx.v
@@ -1,139 +1,89 @@
-/*
-
-Copyright (c) 2014-2017 Alex Forencich
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-
-*/
-
-// Language: Verilog 2001
-
 `timescale 1ns / 1ps
 
-/*
- * AXI4-Stream UART
- */
 module uart_rx #
 (
-    parameter DATA_WIDTH = 8
+     integer G_DATAWIDTH = 8
+    ,integer G_PRESCALE = 1302 // 9600 @ 100 Mhz
 )
 (
-    input  wire                   clk,
-    input  wire                   rst,
-
-    /*
-     * AXI output
-     */
-    output wire [DATA_WIDTH-1:0]  m_axis_tdata,
-    output wire                   m_axis_tvalid,
-    input  wire                   m_axis_tready,
-
-    /*
-     * UART interface
-     */
-    input  wire                   rxd,
-
-    /*
-     * Status
-     */
-    output wire                   busy,
-    output wire                   overrun_error,
-    output wire                   frame_error,
-
-    /*
-     * Configuration
-     */
-    input  wire [15:0]            prescale
-
+     input                    clk
+    ,input                    rst
+    ,output [G_DATAWIDTH-1:0]  uart_rxdata
+    ,output                   uart_rxvalid
+    ,input                    rxd
+    ,output                   busy
+    ,output                   overrun_error
+    ,output                   frame_error
 );
 
-reg [DATA_WIDTH-1:0] m_axis_tdata_reg = 0;
-reg m_axis_tvalid_reg = 0;
+logic [G_DATAWIDTH-1:0] f_tdata = 0;
+logic f_valid = 0;
 
-reg rxd_reg = 1;
+logic f_rxd = 1;
 
-reg busy_reg = 0;
-reg overrun_error_reg = 0;
-reg frame_error_reg = 0;
+logic f_busy = 0;
+logic f_overrun_error = 0;
+logic f_frame_error = 0;
 
-reg [DATA_WIDTH-1:0] data_reg = 0;
-reg [18:0] prescale_reg = 0;
-reg [3:0] bit_cnt = 0;
+logic [G_DATAWIDTH-1:0] f_data = 0;
+logic [18:0] f_prescale = 0;
+logic [3:0] f_bit_cnt = 0;
 
-assign m_axis_tdata = m_axis_tdata_reg;
-assign m_axis_tvalid = m_axis_tvalid_reg;
+assign uart_rxdata = f_tdata;
+assign uart_rxvalid = f_valid;
 
-assign busy = busy_reg;
-assign overrun_error = overrun_error_reg;
-assign frame_error = frame_error_reg;
+assign busy = f_busy;
+assign overrun_error = f_overrun_error;
+assign frame_error = f_frame_error;
 
-always @(posedge clk) begin
-    if (rst) begin
-        m_axis_tdata_reg <= 0;
-        m_axis_tvalid_reg <= 0;
-        rxd_reg <= 1;
-        prescale_reg <= 0;
-        bit_cnt <= 0;
-        busy_reg <= 0;
-        overrun_error_reg <= 0;
-        frame_error_reg <= 0;
+always @(posedge clk) begin : p_uart_rx
+    if (~rst) begin
+        f_tdata <= 0;
+        f_valid <= 0;
+        f_rxd <= 1;
+        f_prescale <= 0;
+        f_bit_cnt <= 0;
+        f_busy <= 0;
+        f_overrun_error <= 0;
+        f_frame_error <= 0;
     end else begin
-        rxd_reg <= rxd;
-        overrun_error_reg <= 0;
-        frame_error_reg <= 0;
+        f_rxd <= rxd;
+        f_overrun_error <= 0;
+        f_frame_error <= 0;
+        f_valid <= 0;
 
-        if (m_axis_tvalid && m_axis_tready) begin
-            m_axis_tvalid_reg <= 0;
-        end
-
-        if (prescale_reg > 0) begin
-            prescale_reg <= prescale_reg - 1;
-        end else if (bit_cnt > 0) begin
-            if (bit_cnt > DATA_WIDTH+1) begin
-                if (!rxd_reg) begin
-                    bit_cnt <= bit_cnt - 1;
-                    prescale_reg <= (prescale << 3)-1;
+        if (f_prescale > 0) begin
+            f_prescale <= f_prescale - 1;
+        end else if (f_bit_cnt > 0) begin
+            if (f_bit_cnt > G_DATAWIDTH+1) begin
+                if (!f_rxd) begin
+                    f_bit_cnt <= f_bit_cnt - 1;
+                    f_prescale <= (G_PRESCALE << 3)-1;
                 end else begin
-                    bit_cnt <= 0;
-                    prescale_reg <= 0;
+                    f_bit_cnt <= 0;
+                    f_prescale <= 0;
                 end
-            end else if (bit_cnt > 1) begin
-                bit_cnt <= bit_cnt - 1;
-                prescale_reg <= (prescale << 3)-1;
-                data_reg <= {rxd_reg, data_reg[DATA_WIDTH-1:1]};
-            end else if (bit_cnt == 1) begin
-                bit_cnt <= bit_cnt - 1;
-                if (rxd_reg) begin
-                    m_axis_tdata_reg <= data_reg;
-                    m_axis_tvalid_reg <= 1;
-                    overrun_error_reg <= m_axis_tvalid_reg;
+            end else if (f_bit_cnt > 1) begin
+                f_bit_cnt <= f_bit_cnt - 1;
+                f_prescale <= (G_PRESCALE << 3)-1;
+                f_data <= {f_rxd, f_data[G_DATAWIDTH-1:1]};
+            end else if (f_bit_cnt == 1) begin
+                f_bit_cnt <= f_bit_cnt - 1;
+                if (f_rxd) begin
+                    f_tdata <= f_data;
+                    f_valid <= 1;
+                    f_overrun_error <= f_valid;
                 end else begin
-                    frame_error_reg <= 1;
+                    f_frame_error <= 1;
                 end
             end
         end else begin
-            busy_reg <= 0;
-            if (!rxd_reg) begin
-                prescale_reg <= (prescale << 2)-2;
-                bit_cnt <= DATA_WIDTH+2;
-                data_reg <= 0;
-                busy_reg <= 1;
+            f_busy <= 0;
+            if (!f_rxd) begin
+                f_prescale <= (G_PRESCALE << 2)-2;
+                f_bit_cnt <= G_DATAWIDTH+2;
+                f_data <= 0;
+                f_busy <= 1;
             end
         end
     end
--- rtl/uart_tx.v
+++ rtl/uart_tx.v
@@ -1,112 +1,62 @@
-/*
-
-Copyright (c) 2014-2017 Alex Forencich
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-
-*/
-
-// Language: Verilog 2001
-
 `timescale 1ns / 1ps
-
-/*
- * AXI4-Stream UART
- */
 module uart_tx #
 (
-    parameter DATA_WIDTH = 8
+     integer G_DATAWIDTH = 8
+    ,integer G_PRESCALE = 1302 // 9600 @ 100 Mhz
 )
 (
-    input  wire                   clk,
-    input  wire                   rst,
-
-    /*
-     * AXI input
-     */
-    input  wire [DATA_WIDTH-1:0]  s_axis_tdata,
-    input  wire                   s_axis_tvalid,
-    output wire                   s_axis_tready,
-
-    /*
-     * UART interface
-     */
-    output wire                   txd,
-
-    /*
-     * Status
-     */
-    output wire                   busy,
-
-    /*
-     * Configuration
-     */
-    input  wire [15:0]            prescale
+    input                    clk
+    ,input                    rst
+    ,input  [G_DATAWIDTH-1:0] uart_txdata
+    ,input                    uart_txvalid
+    ,output                   uart_txready
+    ,output                   txd
+    ,output                   busy
 );
 
-reg s_axis_tready_reg = 0;
+logic f_axis_tready = 0;
+logic f_txd = 1;
+logic f_busy = 0;
+logic [G_DATAWIDTH:0] f_data = 0;
+logic [18:0] f_prescale = 0;
+logic [3:0] f_bit_cnt = 0;
 
-reg txd_reg = 1;
+assign txd = f_txd;
 
-reg busy_reg = 0;
-
-reg [DATA_WIDTH:0] data_reg = 0;
-reg [18:0] prescale_reg = 0;
-reg [3:0] bit_cnt = 0;
-
-assign s_axis_tready = s_axis_tready_reg;
-assign txd = txd_reg;
-
-assign busy = busy_reg;
+assign busy = f_busy;
 
 always @(posedge clk) begin
-    if (rst) begin
-        s_axis_tready_reg <= 0;
-        txd_reg <= 1;
-        prescale_reg <= 0;
-        bit_cnt <= 0;
-        busy_reg <= 0;
+    if (~rst) begin
+        f_axis_tready <= 0;
+        f_txd <= 1;
+        f_prescale <= 0;
+        f_bit_cnt <= 0;
+        f_busy <= 0;
     end else begin
-        if (prescale_reg > 0) begin
-            s_axis_tready_reg <= 0;
-            prescale_reg <= prescale_reg - 1;
-        end else if (bit_cnt == 0) begin
-            s_axis_tready_reg <= 1;
-            busy_reg <= 0;
+        if (f_prescale > 0) begin
+            f_axis_tready <= 0;
+            f_prescale <= f_prescale - 1;
+        end else if (f_bit_cnt == 0) begin
+            f_axis_tready <= 1;
+            f_busy <= 0;
 
-            if (s_axis_tvalid) begin
-                s_axis_tready_reg <= !s_axis_tready_reg;
-                prescale_reg <= (prescale << 3)-1;
-                bit_cnt <= DATA_WIDTH+1;
-                data_reg <= {1'b1, s_axis_tdata};
-                txd_reg <= 0;
-                busy_reg <= 1;
+            if (uart_txvalid) begin
+                f_axis_tready <= !f_axis_tready;
+                f_prescale <= (G_PRESCALE << 3)-1;
+                f_bit_cnt <= G_DATAWIDTH+1;
+                f_data <= {1'b1, uart_txdata};
+                f_txd <= 0;
+                f_busy <= 1;
             end
         end else begin
-            if (bit_cnt > 1) begin
-                bit_cnt <= bit_cnt - 1;
-                prescale_reg <= (prescale << 3)-1;
-                {data_reg, txd_reg} <= {1'b0, data_reg};
-            end else if (bit_cnt == 1) begin
-                bit_cnt <= bit_cnt - 1;
-                prescale_reg <= (prescale << 3);
-                txd_reg <= 1;
+            if (f_bit_cnt > 1) begin
+                f_bit_cnt <= f_bit_cnt - 1;
+                f_prescale <= (G_PRESCALE << 3)-1;
+                {f_data, f_txd} <= {1'b0, f_data};
+            end else if (f_bit_cnt == 1) begin
+                f_bit_cnt <= f_bit_cnt - 1;
+                f_prescale <= (G_PRESCALE << 3);
+                f_txd <= 1;
             end
         end
     end
